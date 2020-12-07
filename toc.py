f36c81b0645faeed2cb97126c19fdaf5b5a5d9b8
beer <huohuo5214@126.com>
2020-12-07 14:07:30 +0800

    add toc script

---
+++ toc.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+import os
+import sys
+
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+
+def main():
+    data = {}
+    for f in os.listdir('.'):
+        if f.startswith('.') or f in ignore_file:
+            continue
+        data[f] = {}
+        for sub_f in os.listdir(f):
+            if sub_f.startswith('.'):
+                continue
+            title = get_title(os.path.join(f, sub_f))
+            data[f][sub_f] = title.decode('utf-8')
+
+    if not data:
+        return
+
+    with open('README.md', 'w') as f:
+        f.write("\n")
+        for key, val in data.items():
+            f.write('## {}'.format(key))
+            f.write("\n")
+            for k, v in val.items():
+                f.write(' - [{}](./{}/{})'.format(key, k, v))
+                f.write("\n")
+            f.write("\n")
+
+
+def get_title(file):
+    with open(file, 'r') as f:
+        for line in f.readlines():
+            if line.startswith('##'):
+                return line.strip().replace('## ', '')
+
+
+ignore_file = {
+    'README.md',
+    'toc.py'
+}
+
+if __name__ == '__main__':
+    main()
