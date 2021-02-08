cd290faf61a557c92939c45647e8f597095013d8
yangyang5214 <beer5214@126.com>
2021-02-08 11:14:40 +0800

    leetcode 1290

---
+++ bricks/1290.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+def getDecimalValue(head):
+    pass
+
+
+if __name__ == '__main__':
+    s = [1, 0, 1, 1]
+    r = 0
+    for _ in s:
+        r = 2 * r + _
+    print(r)
